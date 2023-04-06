1151cad331cd641d85bd49aad59effd9a2b6892e
Besim Hakaj <bh58330@ubt-uni.net>
2023-04-07 00:43:38 +0200

    Rregullim

--- starting-app/src/Login.jsx
+++ starting-app/src/Login.jsx
@@ -8,7 +8,7 @@ export const Login = () => {
 
                 <div className="icona"><RiAccountCircleFill/></div>
             <select name="market" id="">
-                <option value="zgjeth">Zgjethe Marketin</option>
+                <option value="zgjidh">Zgjidhe Marketin</option>
                 <option value="shop1">Shop 1</option>
                 <option value="shop2">Shop 2</option>
                 <option value="shop3">Shop 3</option>
