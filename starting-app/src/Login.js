252eff998af58c4f7cf7bb181a0634602f66246e
Besim Hakaj <bh58330@ubt-uni.net>
2023-04-30 01:33:52 +0200

    fixing

---
+++ starting-app/src/Login.js
@@ -0,0 +1,12 @@
+import {Login} from './pages/Login';
+import './Login.css'
+
+function Log() {
+    return(
+        <div className='Login'>
+            < Login />
+        </div>
+    );
+}
+
+export default Login;
