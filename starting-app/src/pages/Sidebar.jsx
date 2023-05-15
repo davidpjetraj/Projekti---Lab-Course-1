6d6a864e99bbe03cde44f0c7a930d3222c3f1325
Besim Hakaj <bh58330@ubt-uni.net>
2023-05-16 00:30:38 +0200

    sidebar hover and active fixing

--- starting-app/src/pages/Sidebar.jsx
+++ starting-app/src/pages/Sidebar.jsx
@@ -1,17 +1,34 @@
 import { BiBox, BiClipboard, BiHome } from "react-icons/bi"
 import { FiSettings } from "react-icons/fi";
 import { MdPayment } from "react-icons/md";
-import { Link } from "react-router-dom";
+import { Link , useResolvedPath , useMatch, resolvePath } from "react-router-dom";
 
 export const Sidebar = () => {
     return(
-        <div className="sidebari">
 
-            <button> <Link to='/app' className="buttoni"> < BiHome />Home </Link> </button>
-            <button> <Link to='/products' className="buttoni"> < BiClipboard />Products </Link> </button>
-            <button> <Link to='/payment' className="buttoni"> < MdPayment />Payment </Link> </button>
-            <button> <Link to='/order' className="buttoni"> < BiBox />Orders </Link> </button>
-            <button> <Link to='/setting' className="buttoni"> < FiSettings />Settings </Link> </button>
+
+
+        <div className="sidebari">
+            <ul >
+                <CustomLink to='/app' ><li className="buttoni">< BiHome className="icona" />Home</li></CustomLink>
+                <CustomLink to='/products' ><li className="buttoni">< BiClipboard className="icona" />Products</li></CustomLink>
+                <CustomLink to='/payment' ><li className="buttoni">< MdPayment className="icona" />Payment</li></CustomLink>
+                <CustomLink to='/order' ><li className="buttoni">< BiBox className="icona" />Orders</li></CustomLink>
+                <CustomLink to='/setting' ><li className="buttoni">< FiSettings className="icona" />Settings</li></CustomLink>
+                
+            </ul>
         </div>
+    )
+}
+function CustomLink({ to , children, ...props}) {
+    const resolvedPath = useResolvedPath(to)
+    const isActive = useMatch({path: resolvedPath.pathname , end:true})
+
+    return(
+        <li className={isActive ? "active": "" }>
+            <Link to={to} {...props}>
+                {children}
+            </Link>
+        </li>
     )
 }
